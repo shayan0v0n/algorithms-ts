28d1b6c488412e5d42ca4032b06a6cc10a00b997
shayan vosoughi <shayanwqhw@gmail.com>
2024-03-21 23:55:13 +0330

    change BFS example

--- BFS/app.ts
+++ BFS/app.ts
@@ -48,6 +48,7 @@ class Graph {
     while (!queue.isEmpty()) {
       const currentVertex = queue.dequeue();
 
+      console.log(currentVertex, targetVertex);
       if (currentVertex === targetVertex) {
         const shortestPath: number[] = [];
         let vertex = targetVertex;
@@ -81,10 +82,11 @@ class Graph {
 }
 
 new Graph()
-  .addEdge(0, 1)
-  .addEdge(0, 2)
+  .addEdge(1, 2)
   .addEdge(1, 3)
-  .addEdge(1, 4)
-  .addEdge(2, 4)
   .addEdge(3, 4)
-  .bfs(0, 4);
+  .addEdge(4, 5)
+  .addEdge(2, 5)
+  .addEdge(2, 6)
+  .addEdge(6, 7)
+  .bfs(1, 5);
